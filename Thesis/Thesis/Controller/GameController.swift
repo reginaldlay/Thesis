53303ff0325d3510d32db20a0cf86305eeb484b5
reginaldlay <reginaldlay777@gmail.com>
2023-05-14 17:51:23 +0700

    membuat GameScene dan menambahkan beberapa aset

--- Thesis/Thesis/Controller/GameController.swift
+++ Thesis/Thesis/Controller/GameController.swift
@@ -11,7 +11,39 @@ import GameplayKit
 class GameController: SKScene {
     
     override func didMove(to view: SKView) {
+        addNode(imageName: "background", name: "background", widthSize: 750, heightSize: 1334, xPos: 0, yPos: 0, zPos: -1)
+        addNode(imageName: "logo", name: "logo", widthSize: 500, heightSize: 500, xPos: 0, yPos: 0, zPos: 0)
         
+    }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for touch in touches {
+//            let location = touch.location(in: self)
+//            let node = self.atPoint(location)
+//        }
+//    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let node = self.atPoint(location)
+            
+            if (node.name == "touch_screen") {
+                if let nextScene = SKScene(fileNamed: "LandingScene") {
+                    self.scene?.scaleMode = .aspectFill
+                    self.scene?.view?.presentScene(nextScene)
+                }
+            }
+        }
+    }
+    
+    func addNode(imageName: String, name: String, widthSize: CGFloat, heightSize: CGFloat, xPos: CGFloat, yPos: CGFloat, zPos: CGFloat) {
+        let homeAsset = SKSpriteNode(imageNamed: imageName)
+        homeAsset.name = name
+        homeAsset.size = CGSize(width: widthSize, height: heightSize)
+        homeAsset.position = CGPoint(x: xPos, y: yPos)
+        homeAsset.zPosition = zPos
+        addChild(homeAsset)
     }
     
     override func update(_ currentTime: TimeInterval) {
