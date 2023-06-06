c94346e8454cec4527d87ed9e5c0e08a03160ce3
reginaldlay <reginaldlay777@gmail.com>
2023-06-06 18:40:59 +0700

    rapirapi4

--- Thesis/Thesis/Controller/PlayController.swift
+++ Thesis/Thesis/Controller/PlayController.swift
@@ -12,14 +12,12 @@ class PlayController: ConstructController {
     
     override func didMove(to view: SKView) {
         addImage(imageName: "background_base", name: "background_base", widthSize: 390, heightSize: 844, xPos: 0, yPos: 0, zPos: -1)
-        addImage(imageName: "background_header", name: "background_header", widthSize: 125, heightSize: 120, xPos: -132.5, yPos: 327.5, zPos: 0)
-        addImage(imageName: "background_subheader", name: "background_subheader", widthSize: 275, heightSize: 120, xPos: 57.5, yPos: 327.5, zPos: 0)
-        addImage(imageName: "button_back", name: "button_back", widthSize: 55, heightSize: 50, xPos: -140, yPos: 230, zPos: 0)
-        addImage(imageName: "background_level_number", name: "background_level_number", widthSize: 200, heightSize: 120, xPos: 0, yPos: -44, zPos: 0)
-        addImage(imageName: "button_previous", name: "button_previous", widthSize: 48, heightSize: 48, xPos: -135, yPos: -44, zPos: 0)
-        addImage(imageName: "button_next", name: "button_next", widthSize: 48, heightSize: 48, xPos: 135, yPos: -44, zPos: 0)
-        addLabel(fontName: "Futura Medium", name: "level_name", text: "Level 1", fontSize: 25, fontColor: .black, xAlignment: .center, xPos: 0, yPos: -50, zPos: 1)
-        addLabel(fontName: "Futura Medium", name: "level_choose", text: "Choose your level!", fontSize: 32, fontColor: .black, xAlignment: .center, xPos: 0, yPos: 80, zPos: 0)
+        addImage(imageName: "button_back", name: "button_back", widthSize: 55, heightSize: 50, xPos: -140, yPos: 320, zPos: 0)
+        addImage(imageName: "background_level_number", name: "background_level_number", widthSize: 200, heightSize: 120, xPos: 0, yPos: 0, zPos: 0)
+        addImage(imageName: "button_previous", name: "button_previous", widthSize: 48, heightSize: 48, xPos: -135, yPos: 0, zPos: 0)
+        addImage(imageName: "button_next", name: "button_next", widthSize: 48, heightSize: 48, xPos: 135, yPos: 0, zPos: 0)
+        addLabel(fontName: "Futura Medium", name: "level_name", text: "Level 1", fontSize: 25, fontColor: .black, xAlignment: .center, xPos: 0, yPos: -6, zPos: 1)
+        addLabel(fontName: "Futura Medium", name: "level_choose", text: "Choose your level!", fontSize: 30, fontColor: .black, xAlignment: .center, xPos: 0, yPos: 124, zPos: 0)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
